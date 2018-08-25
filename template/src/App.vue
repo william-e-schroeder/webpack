73475c182dda29ba69339e78cc28a64368cbf251
william eric schroeder <williamericschroeder@gmail.com>
2018-08-25 14:19:57 -0500

    Change defualts to Vuetify Webpack template
    Change defualts to Vuetify Webpack template to match organization needs

--- template/src/App.vue
+++ template/src/App.vue
@@ -3,7 +3,7 @@
     <v-navigation-drawer
       persistent
       :mini-variant="miniVariant"
-      :clipped="clipped"
+      :clipped="!clipped"
       v-model="drawer"
       enable-resize-watcher
       fixed
@@ -11,71 +11,44 @@
     >
       <v-list>
         <v-list-tile
-          value="true"
+          value="false"
           v-for="(item, i) in items"
           :key="i"
+          :to="{name: item.namedRoute}"
         >
           <v-list-tile-action>
             <v-icon v-html="item.icon"></v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+            <v-list-tile-title class="accent--text" v-text="item.title"></v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar
       app
-      :clipped-left="clipped"
+      :clipped-left="!clipped"
     >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
       </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <img src="@/assets/header-logo.png">
+      <v-toolbar-title class="accent--text" v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
     </v-toolbar>
     <v-content>
       {{#router}}
       <router-view/>
       {{else}}
-      <HelloWorld/>
+      <Dashboard/>
       {{/router}}
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
-    </v-footer>
   </v-app>
 </template>
 
 <script>
 {{#unless router}}
-import HelloWorld from './components/HelloWorld'
+import Dashboard from './components/Dashboard'
 
 {{/unless}}
 export default {
@@ -85,8 +58,9 @@ export default {
       drawer: true,
       fixed: false,
       items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire'
+        icon: 'dashboard',
+        title: 'Dashboard',
+        namedRoute: 'Dashboard'
       }],
       miniVariant: false,
       right: true,
@@ -96,7 +70,10 @@ export default {
   },
   name: 'App'{{#router}}{{else}},
   components: {
-    HelloWorld
+    Dashboard
   }{{/router}}
 }
 </script>
+
+<style>
+</style>
